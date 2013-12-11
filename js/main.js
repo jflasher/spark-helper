dd4283a0b8000850ca7bcfcb4e75a96737b64516
Joseph Flasher <flasher@istrategylabs.com>
2013-12-10 22:13:14 -0500

    Variables and info messages hooked up.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+  var baseURL = "https://api.spark.io/v1/devices/";
+  var timer1, timer2, timer3, timer4;
+
   // Change settings header if we don't have info
   checkSettings();
 
@@ -32,48 +35,36 @@ $(document).ready(function() {
     }
   }
 
-  // Turn on/off the variable refresh if box is checked
-  $("#refresh-1").on("change", function () {
-    if($("#refresh-1").is(":checked")) {
-      $("#get-var-1").attr("disabled", "disabled");
-    } else {
-      $("#get-var-1").removeAttr("disabled");
-    }
-  });
-  $("#refresh-2").on("change", function () {
-    if($("#refresh-2").is(":checked")) {
-      $("#get-var-2").attr("disabled", "disabled");
-    } else {
-      $("#get-var-2").removeAttr("disabled");
-    }
-  });
-  $("#refresh-3").on("change", function () {
-    if($("#refresh-3").is(":checked")) {
-      $("#get-var-3").attr("disabled", "disabled");
-    } else {
-      $("#get-var-3").removeAttr("disabled");
-    }
-  });
-  $("#refresh-4").on("change", function () {
-    if($("#refresh-4").is(":checked")) {
-      $("#get-var-4").attr("disabled", "disabled");
-    } else {
-      $("#get-var-4").removeAttr("disabled");
-    }
-  });
+  ////
+  // Alerts
+  ////
+  $("#info-alert").alert();
 
+  ////
+  // Methods
+  ////
   // On method success
   function onMethodSuccess() {
-    console.log("success");
+    alert = $("#info-alert");
+    alert.text("Success!").removeClass("alert-danger").addClass("alert-success");
+    alert.show();
+    setTimeout(function() {
+      alert.hide();
+    }, 2000);
   }
 
   function onMethodFailure() {
-    console.log("fail");
+    alert = $("#info-alert");
+    alert.text("Ruh roh!").removeClass("alert-success").addClass("alert-danger");
+    alert.show();
+    setTimeout(function() {
+      alert.hide();
+    }, 2000);
   }
 
   // The base level run method command
   function doMethod(method, data) {
-    var url = "https://api.spark.io/v1/devices/" + $("#core-id").val() + "/" + method;
+    var url = baseURL + $("#core-id").val() + "/" + method;
     $.ajax({
       type: "POST",
       url: url,
@@ -98,4 +89,112 @@ $(document).ready(function() {
   $("#post-4").on("click", function () {
     doMethod($("#method-4").val(), $("#data-4").val());
   });
+
+  ////
+  // Variables
+  ////
+  // Generic get method
+  function getVariable(variable, callback) {
+    var url = baseURL + $("#core-id").val() + "/" + variable + "?access_token=" + $("#api-token").val();
+    $.getJSON(url, callback)
+      .fail(function(obj) {
+        onMethodFailure();
+      });
+  }
+
+  // Variable on clicks
+  $("#get-var-1").on("click", function () {
+    getVariable1();
+  });
+  $("#get-var-2").on("click", function () {
+    getVariable2();
+  });
+  $("#get-var-3").on("click", function () {
+    getVariable3();
+  });
+  $("#get-var-4").on("click", function () {
+    getVariable4();
+  });
+
+  // Get variable methods
+  function getVariable1() {
+    getVariable($("#var-1").val(), function (res) {
+      $("#var-val-1").val(res.result);
+    });
+  }
+  function getVariable2() {
+    getVariable($("#var-2").val(), function (res) {
+      $("#var-val-2").val(res.result);
+    });
+  }
+  function getVariable3() {
+    getVariable($("#var-3").val(), function (res) {
+      $("#var-val-3").val(res.result);
+    });
+  }
+  function getVariable4() {
+    getVariable($("#var-4").val(), function (res) {
+      $("#var-val-4").val(res.result);
+    });
+  }
+
+  // Auto-refresh
+  // Turn on/off the variable refresh if box is checked
+  $("#refresh-1").on("change", function () {
+    if($("#refresh-1").is(":checked")) {
+      var time = ($("#refresh-time-1").val() === '') ? 5000 : $("#refresh-time-1").val();
+      $("#get-var-1").attr("disabled", "disabled");
+      timer1 = setInterval(function () {
+        getVariable1();
+      }, time);
+    } else {
+      $("#get-var-1").removeAttr("disabled");
+      if (timer1) {
+        clearInterval(timer1);
+      }
+    }
+  });
+  $("#refresh-2").on("change", function () {
+    if($("#refresh-2").is(":checked")) {
+      var time = ($("#refresh-time-2").val() === '') ? 5000 : $("#refresh-time-2").val();
+      $("#get-var-2").attr("disabled", "disabled");
+      timer2 = setInterval(function () {
+        getVariable2();
+      }, time);
+    } else {
+      $("#get-var-2").removeAttr("disabled");
+      if (timer2) {
+        clearInterval(timer2);
+      }
+    }
+  });
+  $("#refresh-3").on("change", function () {
+    if($("#refresh-3").is(":checked")) {
+      var time = ($("#refresh-time-3").val() === '') ? 5000 : $("#refresh-time-3").val();
+      $("#get-var-3").attr("disabled", "disabled");
+      timer3 = setInterval(function () {
+        getVariable3();
+      }, time);
+    } else {
+      $("#get-var-3").removeAttr("disabled");
+      if (timer3) {
+        clearInterval(timer3);
+      }
+    }
+  });
+  $("#refresh-4").on("change", function () {
+    if($("#refresh-4").is(":checked")) {
+      var time = ($("#refresh-time-4").val() === '') ? 5000 : $("#refresh-time-4").val();
+      $("#get-var-4").attr("disabled", "disabled");
+      timer4 = setInterval(function () {
+        getVariable4();
+      }, time);
+    } else {
+      $("#get-var-4").removeAttr("disabled");
+      if (timer4) {
+        clearInterval(timer4);
+      }
+    }
+  });
+
 });
